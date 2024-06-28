95bdc5df557e8c722f7dbe7c7804a7d162cdadf2
Nitheshwaran <nitheshwaran003@gmail.com>
2024-06-28 11:55:52 +0530

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import './assets/style.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    home: new MyApp(),
+  runApp(MaterialApp(
+    home: const MyApp(),
+    theme:ThemeData(useMaterial3: true),
     debugShowCheckedModeBanner: false,
   ));
 }
@@ -14,13 +16,48 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final List<Map<String, dynamic>> _navItems = [
+    {'icon': Icons.home, 'label': 'Home'},
+    {'icon': Icons.chat, 'label': 'Chat'},
+    {'icon': Icons.settings, 'label': 'Settings'},
+    // {'icon': Icons.post_add, 'label': 'Post'},
+    // {'icon': Icons.settings, 'label': 'Settings'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Skisho'),
         backgroundColor: Color(0x00000000),
+        leading: SafeArea(
+          child: CircleAvatar(
+            backgroundColor: Colors.brown.shade800,
+            child: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notification_add_outlined),
+            color: AppColors.primaryColor,
+            onPressed: () {
+              print('Navigate page');
+            },
+          ),
+        ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: _navItems.map((item) {
+        return BottomNavigationBarItem(
+            icon: Icon(
+              item['icon'],
+              // color: AppColors.primaryColor,
+            ),
+            label: item['label']);
+      }).toList()),
     );
   }
 }
