34ee41166c7126c9a10d2805def9e9579ba99d41
Nitheshwaran <nitheshwaran003@gmail.com>
2024-06-28 10:28:27 +0530

    SkiskoApp

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Skisho'),
+        backgroundColor: Color(0x00000000),
+      ),
+    );
+  }
+}
